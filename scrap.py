6a97a199f74a10787d5e08de1e6ce7d823848981
Daniel Munoz <146023389+dmunozbatista@users.noreply.github.com>
2024-03-25 22:39:12 -0500

    Adding documentation

--- scrap.py
+++ scrap.py
@@ -2,6 +2,8 @@ import lxml.html
 import time
 import requests
 from urllib.parse import urlparse
+from clean_data import clean_league_name, clean_team_name
+
 
 REQUEST_DELAY = 0.9
 url = "https://fbref.com/en/comps/12/La-Liga-Stats"
@@ -42,6 +44,15 @@ def make_link_absolute(rel_url, current_url):
     
 
 def get_team_links(url):
+    """
+    Use xpaths to retrieve all team links. It only requires the web page main link
+
+    Input:
+        url (str): main webpage with football stats of La Liga
+    
+    Return:
+        team_lst (list): list with all teams url of La Liga
+    """
     standings_page = make_request(url).text
     root = lxml.html.fromstring(standings_page)
     all_teams_elements = root.xpath('//*[@id="results2023-2024121_overall"]/tbody/tr/td[1]/a')
@@ -55,9 +66,74 @@ def get_team_links(url):
 
 teams_urls = get_team_links(url)
 
-url = "https://fbref.com/en/squads/53a2f082/Real-Madrid-Stats"
-page = make_request(url).text
-root = lxml.html.fromstring(page)
-team_items = root.xpath('//*[@id="meta"]/div/h1/span')
-league_text = team_items[1]
-team_name = team_items[0]
+def get_player_data(url):
+    """
+    Given a team url, get all the players relevant data using xpaths
+
+    Input:
+        url (str): team url
+    
+    Return:
+        player_data (list): list of dictionaries to easily transform to dataframe
+    """
+    page = make_request(url).text
+    root = lxml.html.fromstring(page)
+    team_items = root.xpath('//*[@id="meta"]/div/h1/span')
+    league_text = team_items[1].text
+    team_name = team_items[0].text
+    cleaned_league = clean_league_name(league_text)
+    cleaned_name = clean_team_name(team_name)
+
+    players = root.xpath('//*[@id="stats_standard_12"]//tbody//tr//th[@class="left "]')  # XPath to target the player elements
+
+    player_data = []
+    for player in players:
+        player_root = player.xpath('./following-sibling::td')
+        player_name = player[0].text.strip()  # Extract the text content of the player element and remove leading/trailing spaces
+        mins = player_root[5].text
+        npxg = player_root[16].text
+        xa = player_root[17].text
+        xg = player_root[15].text
+        prgc = player_root[19].text # XPath to extract PrgC
+        prgp = player_root[20].text  # XPath to extract PrgP
+        prgr = player_root[21].text
+        position = player_root[1].text
+        age = player_root[2].text
+
+        player_data.append({
+            "team": cleaned_name,
+            "league": cleaned_league,
+            "name": player_name,
+            "position": position,
+            "npxg": npxg,
+            "prgc": prgc,
+            "prgp": prgp,
+            "prgr": prgr,
+            "xg": xg,
+            "mins": mins,
+            "age": age,
+            "xa": xa
+        })
+    return player_data
+
+
+def get_la_liga(url_la_liga):
+    """
+    Give La liga url and retrieve all the information. It works for differerent
+    leagues too
+
+    Inputs:
+        url_la_liga (str): La liga url
+
+    Return:
+        complete_la_liga_data (list): each element is a team data
+    """
+    all_teams_url = get_team_links(url_la_liga)
+    complete_la_liga_data = []
+    for url_team in all_teams_url:
+        team = get_player_data(url_team)
+        complete_la_liga_data += team
+    return complete_la_liga_data
+        
+la_liga_data = get_la_liga("https://fbref.com/en/comps/12/La-Liga-Stats")
+
